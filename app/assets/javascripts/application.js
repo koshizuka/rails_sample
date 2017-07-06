0e4d3e049450e6434291062934cdc8eb0238cdd0
koshizuka <koshizuka@akibare.net>
2017-07-06 13:20:59 +0900

    nicoテーマをあてる

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require _honoka
+//= require _honoka.min
 //= require jquery-fileupload/basic
 //= require dropzone
 //= require_tree .
